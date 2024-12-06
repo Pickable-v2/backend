b6310523e4193859bb0b44ca4d59798382f2e2c4
Jihyun3478 <leejihyun3478@gmail.com>
2024-12-06 21:20:19 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/example/eatpick/EatPickApplication.java
@@ -0,0 +1,13 @@
+package com.example.eatpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatPickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatPickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eatpick/EatPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eatpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatPickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
