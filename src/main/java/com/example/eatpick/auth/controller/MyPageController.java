a0001de826b67ea61bc1546176453ae6d81cd877
Jihyun3478 <leejihyun3478@gmail.com>
2024-12-23 15:48:44 +0900

    fix: 잘못된 어노테이션 수정

--- src/main/java/com/example/eatpick/auth/controller/MyPageController.java
+++ src/main/java/com/example/eatpick/auth/controller/MyPageController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.eatpick.auth.domain.dto.request.MyPageUpdateRequest;
@@ -15,7 +16,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@RestController("/mypage")
+@RestController
+@RequestMapping("/mypage")
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "Member", description = "Member API")
